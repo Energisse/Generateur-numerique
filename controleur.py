bda995fd262b0da0fa94135b0826b4ba30792d7f
Energisse <thomas.halvick@gmail.com>
2022-03-17 16:46:23 +0100

    ajout addition de formule et correction de sybntax

--- controleur.py
+++ controleur.py
@@ -2,12 +2,12 @@ from datetime import datetime, timedelta
 import os
 class Controleur:
     def __init__(self,taille = 10,periode = 1):
-        self.listeFormule = []
+        self.liste_formule = []
         self.periode = periode
         self.taille = taille
     
     def addFormule(self,formule):
-        self.listeFormule.append(formule)
+        self.liste_formule.append(formule)
 
     def calcul(self,nom_fichier):
         
@@ -19,7 +19,7 @@ class Controleur:
         date = datetime.now()
         
         entete = "date,"
-        for index in range(len(self.listeFormule)):
+        for index in range(len(self.liste_formule)):
                 entete += str(index)+","
         entete = entete[:-1]+"\n"; 
         f.write(entete) 
@@ -27,9 +27,9 @@ class Controleur:
         for x in range(self.taille):
             texte = str(date.strftime("%H:%M:%S %Y/%m/%d"))+","   
             
-            for formule in self.listeFormule:
+            for formule in self.liste_formule:
                 texte += str(str(formule.calcul(x/self.periode)) +",")
-            texte = texte[:-1]+"\n";
+            texte = texte[:-1]+"\n"
             f.write(texte) 
             
             date +=  timedelta(seconds=self.periode)
--- formule.py
+++ formule.py
@@ -8,19 +8,14 @@ class Formule:
         self.periode = periode
         self.amplitude = amplitude
         self.bruit = bruit
-        self.listeFormule = []
+        self.liste_formule = []
 
     def calcul(self,x):
         bruit= self.bruit*(random.random()-0.5)
         y = math.sin(math.pi*2*(x/self.periode))*self.amplitude
-        for formule in self.listeFormule:
+        for formule in self.liste_formule:
             y += formule.calcul(x)
         return y + bruit
-    
-    def setPeriode(self,periode):
-        if(periode <= 0):
-            raise ValueError('La periode doit être superieur a 0')
-        self.periode = periode
 
     def addFormule(self,formule):
-        self.listeFormule.append(formule)
+        self.liste_formule.append(formule)
