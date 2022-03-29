50c390c60aa590e32032f38259613f5518849b0a
Energisse <61659170+Energisse@users.noreply.github.com>
2022-03-29 16:29:10 +0200

    modification de la generation du fichier csv

--- controleur.py
+++ controleur.py
@@ -1,36 +1,39 @@
 from datetime import datetime, timedelta
 import os
+import pandas as pd
+import numpy as np
+
 class Controleur:
     def __init__(self,taille = 10,periode = 1):
         self.liste_formule = []
         self.periode = periode
         self.taille = taille
+        self.data_frame = None
     
     def addFormule(self,formule):
         self.liste_formule.append(formule)
 
-    def calcul(self,nom_fichier):
-        
-        if os.path.exists(nom_fichier):
-            os.remove(nom_fichier)
-            
-        f = open(nom_fichier, "a")
-        
+    def calcul(self):
+        liste_mesure = []
+        liste_date = []
+
         date = datetime.now()
-        
-        entete = "date,"
-        for index in range(len(self.liste_formule)):
-                entete += str(index)+","
-        entete = entete[:-1]+"\n"; 
-        f.write(entete) 
-        
         for x in range(self.taille):
-            texte = str(date.strftime("%H:%M:%S %Y/%m/%d"))+","   
-            
+            liste_mesure_ligne = []
+
             for formule in self.liste_formule:
-                texte += str(str(formule.calcul(x/self.periode)) +",")
-            texte = texte[:-1]+"\n"
-            f.write(texte) 
-            
+                liste_mesure_ligne.append(formule.calcul(x/self.periode))
+
             date +=  timedelta(seconds=self.periode)
-        f.close()
+
+            liste_date.append(date.strftime("%H:%M:%S %Y/%m/%d"))
+            liste_mesure.append(liste_mesure_ligne)
+            
+        self.data_frame = pd.DataFrame(liste_mesure, index = liste_date)
+        
+
+    def enregistrer_en_csv(self,nom_fichier):
+        if os.path.exists(nom_fichier):
+             os.remove(nom_fichier)
+       
+        self.data_frame.to_csv(nom_fichier)
