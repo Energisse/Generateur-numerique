06edfc21933d918485c629738d083d4af917ee5f
Thomas Halvick <61659170+Energisse@users.noreply.github.com>
2022-04-02 15:55:35 +0200

    Correction et afout de la fonction affine

--- formule_affine.py
+++ formule_affine.py
@@ -7,7 +7,7 @@ class FormuleAffine:
     
     def __init__(self,coef_direc = 1,y = 0):
         self.coef_direc = coef_direc
-        self.y = Y
+        self.y = y
 
     def calcul(self,x):
         return self.y+self.coef_direc*x
